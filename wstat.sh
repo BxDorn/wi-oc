600fa5b3ce4ad2949b86a188c4f8df35d16377e9
Tim Bleidorn <tim.bleidorn@outlook.com>
2020-07-21 22:38:44 -0600

    swtatus update

--- wstat.sh
+++ wstat.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-
+clear
 echo "WOC Status"
 echo ------------------------------------------------------
-isPrimaryConfig=$(sed -n '2{p;q;}' load_var.woc)
-
-if [[ isPrimaryConfig != "Primary" ]]; then
-	echo "Unit Configuration = Standby"
-else
-	echo "Init Configuration = Primary"
-fi
-
+isPrimaryConfig=$(sed -n '2{p;q;}' /root/wi-oc/load_var.woc)
+echo "unit configured as" $isPrimaryConfig
 opTag=$( tail -n 1 woc_status.woc )
-
 echo "Unit Operational State =" $opTag
-
-ip link show br0 | grep UP
-ip link show BNG1 | grep UP
-ip link show ens224 | grep UP
-
-
+echo ------------------------------------------------------
+ip link show
+echo ------------------------------------------------------
 trackedMac=$(bridge fdb show | wc -l)
 echo "total tracked MAC addresses = " $trackedMac
 echo ------------------------------------------------------
